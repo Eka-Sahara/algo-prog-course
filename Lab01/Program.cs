5b6874de1e72a7b1123c1aa77df39891ae57eebc
Eka-Sahara <ekaterinasahabutdinova473@gmail.com>
2026-09-10 20:31:15 +0300

    Lab01: strings, contants, comments

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -19,7 +19,7 @@ double roomArea = roomWidth * roomLength;
 double roomPerimeter = (roomWidth + roomLength) * 2;
 System.Console.WriteLine($"Ширина: {roomWidth} м, длинна: {roomLength} м");
 System.Console.WriteLine($"Площадь: {roomArea} кв.м");
-System.Console.WriteLine($"Периметр: {roomPerimeter} м");
+System.Console.WriteLine($"Периметр: {roomPerimeter} м");       //расчитывает площадь и периметр комнаты
 
 
 System.Console.WriteLine();
@@ -31,7 +31,9 @@ double totalWithInterest = laptopPrice * (1 + interestRate);
 double monthlyPayment = totalWithInterest / monthsCount;
 System.Console.WriteLine($"Цена ноутбука: {laptopPrice} руб.");
 System.Console.WriteLine($"Итого с процентами: {totalWithInterest} руб.");
-System.Console.WriteLine($"Платёж в месяц: {monthlyPayment} руб.");
+System.Console.WriteLine($"Платёж в месяц: {monthlyPayment} руб.");     //рассчитывает цену самого ноутбука, цену с процентами и сколько рублей в месяц выплачивать
+
+
 System.Console.WriteLine();
 System.Console.WriteLine("Внимание деление int");
 int totalStudents = 25;
@@ -40,3 +42,26 @@ int studentsPerGroupWrong = totalStudents / groupsCount;
 double studentsPerGroupCorrect = (double)totalStudents / groupsCount;
 System.Console.WriteLine($"25 / 4 как int: {studentsPerGroupWrong}");
 System.Console.WriteLine($"25 / 4 как int: {studentsPerGroupCorrect}");
+
+
+System.Console.WriteLine();
+System.Console.WriteLine("Способы собрать строку");
+string firstName = "Екатерина";
+string lastName = "Сахабутдинова";
+string fullNameConcat = firstName + " " + lastName;
+string fullNameInterp = $"{firstName} {lastName}";
+string fullNameConcatMethod = string.Concat(firstName, " ", lastName);
+System.Console.WriteLine(fullNameConcat);
+System.Console.WriteLine(fullNameInterp);
+System.Console.WriteLine(fullNameConcatMethod);
+System.Console.WriteLine($"Все три строки равны:{fullNameConcat == fullNameInterp && fullNameInterp == fullNameConcatMethod}");
+
+System.Console.WriteLine();
+System.Console.WriteLine("Константы");
+const double VatRate = 0.20;
+const string collegeName = "ВФ ВолГУ";
+double productPrice = 1000;
+double priceWithVat = productPrice * (1 + VatRate);
+System.Console.WriteLine($"Учебное заведение: {collegeName}");
+System.Console.WriteLine($"Цена без НДС: {productPrice}, с НДС ({VatRate:P0}):{priceWithVat}");      //находит стоимость обучения в колледже
+
