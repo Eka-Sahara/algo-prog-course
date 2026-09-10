8b879a4b3a0d0b9be99df7463a3e31e2266d60e8
Eka-Sahara <ekaterinasahabutdinova473@gmail.com>
2026-09-10 23:19:29 +0300

    Lab01 task 2: temperature converter

---
+++ TempConverter/Program.cs
@@ -0,0 +1,5 @@
+﻿double celsius = 18.2;
+const double num = 273.15;
+double farengeith = celsius * 9 / 5 + 32;
+double kelvin = celsius + num;
+System.Console.WriteLine($"{celsius}, ={farengeith}F, ={kelvin}K");
